299f9f1622ec156a33925d95ad0a2904655cc55c
Florian Beiser <florian.beiser@sintef.no>
2021-07-15 13:10:36 +0200

    plot with fixed axis limits

--- Statistics.py
+++ Statistics.py
@@ -3,27 +3,35 @@ Mean and Variance for the advection diffusion example
 """
 
 from Simulator import Simulator
+from Ensemble import Ensemble
+
+
 import numpy as np
-from random import random
 
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 
 class Statistics:
-    def __init__(self, simulator, ensemble_flag=False):
+    def __init__(self, simulator, ensemble=0):
         """Class for handling the mean and cov throughout times"""
         self.simulator = simulator
         print("Please remember to set priors!")
         self.mean = np.zeros([self.simulator.grid.N_x])
         self.var  = np.zeros([self.simulator.grid.N_x])
         self.cov  = np.zeros([self.simulator.grid.N_x,self.simulator.grid.N_x])
-
+        # Default is analytical 
+        if ensemble > 0:
+            self.ensemble = Ensemble.Ensemble(simulator, ensemble)
+        else:
+            self.ensemble = None
 
     def set(self, mean, cov):
         """Setting the member variables from input arguments"""
         self.mean = mean
         self.var = np.diag(cov)
         self.cov = cov
+        if self.ensemble is not None:
+            self.ensemble.initialize(mean, cov)
 
 
     def plot(self):
@@ -31,20 +39,20 @@ class Statistics:
         fig, axs = plt.subplots(1,3, figsize=(12,4))
 
         mean = np.reshape(self.mean, (self.simulator.grid.ny,self.simulator.grid.nx))
-        fig0 = axs[0].imshow(mean, origin = "lower")
+        fig0 = axs[0].imshow(mean, origin = "lower", vmin=0.0, vmax=0.5)
         axs[0].set_title("Mean")
         ax_divider = make_axes_locatable(axs[0])
         ax_cb = ax_divider.append_axes("bottom", size="10%", pad="20%")
         plt.colorbar(fig0, cax=ax_cb, orientation="horizontal")
 
         var = np.reshape(self.var, (self.simulator.grid.ny,self.simulator.grid.nx))
-        fig1 = axs[1].imshow(var, origin = "lower")
+        fig1 = axs[1].imshow(var, origin = "lower", vmin=0.0, vmax=0.5)
         axs[1].set_title("Variance")
         ax_divider = make_axes_locatable(axs[1])
         ax_cb = ax_divider.append_axes("bottom", size="10%", pad="20%")
         plt.colorbar(fig1, cax=ax_cb, orientation="horizontal")
 
-        fig2 = axs[2].imshow(self.cov)
+        fig2 = axs[2].imshow(self.cov, vmin=0.0, vmax=0.25)
         axs[2].set_title("Covariance Matrix")
         ax_divider = make_axes_locatable(axs[2])
         ax_cb = ax_divider.append_axes("bottom", size="10%", pad="20%")
