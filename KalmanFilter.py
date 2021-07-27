fb62657a169e0ea349400677753fced07b1019d3
Florian Beiser <florian.beiser@sintef.no>
2021-07-27 15:38:15 +0200

    KalmanFilter allowing for serial processing - from neumannBC branch

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -15,16 +15,24 @@ class Kalman:
         self.H = observation.H
         self.tau = observation.noise
 
-    def filter(self, forecasted_mean, forecasted_cov, obs):
+    def filter(self, forecasted_mean, forecasted_cov, obs, series=None):
 
-        Q = np.matmul(self.H,np.matmul(forecasted_cov,self.H.transpose())) + self.tau
-        K = np.matmul(forecasted_cov,np.matmul(self.H.transpose(), np.linalg.inv(Q)))
+        if series is not None:
+            H = self.H[series,:]
 
-        updated_mean = forecasted_mean + np.matmul(K, (obs - np.matmul(self.H,forecasted_mean)))
-        updated_covariance = forecasted_cov - np.matmul(K,np.matmul(Q,K.transpose()))
+            Q = np.matmul(H,np.matmul(forecasted_cov,H.transpose())) + self.tau[series,series]
+            K = np.matmul(forecasted_cov,H.transpose()) / Q
+
+            updated_mean = forecasted_mean + K * (obs - np.matmul(H,forecasted_mean))
+            updated_covariance = forecasted_cov -  Q * np.outer(K, K)
+
+        else:
+            Q = np.matmul(self.H,np.matmul(forecasted_cov,self.H.transpose())) + self.tau
+            K = np.matmul(forecasted_cov,np.matmul(self.H.transpose(), np.linalg.inv(Q)))
+
+            updated_mean = forecasted_mean + np.matmul(K, (obs - np.matmul(self.H,forecasted_mean)))
+            updated_covariance = forecasted_cov - np.matmul(K,np.matmul(Q,K.transpose()))
         
         self.statistics.set(updated_mean, updated_covariance)
 
         return updated_mean, updated_covariance
-
-
