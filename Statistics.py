FFT for inital ensemble (variance wrong scaled)

Statistics.py
@@ -33,25 +33,29 @@ class Statistics:
             self.ensemble.initialize(mean, cov, var_mesh)
 
 
-    def plot(self):
+    def plot(self, mean=None, var=None, cov=None):
         """Plotting mean, var, and cov in a unified graphics"""
         fig, axs = plt.subplots(1,3, figsize=(12,4))
 
-        mean = np.reshape(self.mean, (self.simulator.grid.ny,self.simulator.grid.nx))
+        if mean is None:
+            mean = np.reshape(self.mean, (self.simulator.grid.ny,self.simulator.grid.nx))
         fig0 = axs[0].imshow(mean, origin = "lower", vmin=0.0, vmax=0.5)
         axs[0].set_title("Mean")
         ax_divider = make_axes_locatable(axs[0])
         ax_cb = ax_divider.append_axes("bottom", size="10%", pad="20%")
         plt.colorbar(fig0, cax=ax_cb, orientation="horizontal")
 
-        var = np.reshape(self.var, (self.simulator.grid.ny,self.simulator.grid.nx))
+        if var is None:
+            var = np.reshape(self.var, (self.simulator.grid.ny,self.simulator.grid.nx))
         fig1 = axs[1].imshow(var, origin = "lower", vmin=0.0, vmax=0.5)
         axs[1].set_title("Variance")
         ax_divider = make_axes_locatable(axs[1])
         ax_cb = ax_divider.append_axes("bottom", size="10%", pad="20%")
         plt.colorbar(fig1, cax=ax_cb, orientation="horizontal")
 
-        fig2 = axs[2].imshow(self.cov, vmin=0.0, vmax=0.25)
+        if cov is None:
+            cov = self.cov
+        fig2 = axs[2].imshow(cov, vmin=0.0, vmax=0.25)
         axs[2].set_title("Covariance Matrix")
         ax_divider = make_axes_locatable(axs[2])
         ax_cb = ax_divider.append_axes("bottom", size="10%", pad="20%")