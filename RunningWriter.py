7ecc4594372d90b6f91e68fb1052729ea17a0a20
FlorianBeiser <florian.beiser@sintef.no>
2021-08-23 16:11:37 +0000

    Multiple models in run

--- RunningWriter.py
+++ RunningWriter.py
@@ -16,11 +16,10 @@ class RunningWriter:
         self.ecdf_err_letkfs  = np.zeros((N_poi, trials))
 
 
-    @staticmethod
-    def header2file(N_e, trails_truth, trails_init, timestamp=None):
+    def header2file(self, N_e, trails_truth, trails_init, timestamp=None):
         if timestamp is not None:
-            result_timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
-            file = "experiment_files/experiment_" + timestamp + "/results_" + result_timestamp
+            self.result_timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+            file = "experiment_files/experiment_" + timestamp + "/results_" + self.result_timestamp
 
             f = open(file, "a")
             f.write("--------------------------------------------\n")
@@ -49,28 +48,44 @@ class RunningWriter:
                 avg_ecdf_err_etkfs, avg_ecdf_err_letkfs
 
 
-    def results2file(self, timestamp=None):
+    def results2file(self, timestamp=None, as_table=None):
 
         avg_mean_rmse_etkfs, avg_mean_rmse_letkfs, \
         avg_cov_frob_etkfs, avg_cov_frob_letkfs, \
         avg_ecdf_err_etkfs, avg_ecdf_err_letkfs = self.results()
         
         if timestamp is not None:
-            result_timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
-            file = "experiment_files/experiment_" + timestamp + "/results_" + result_timestamp
 
-            f = open(file, "a")
-            f.write("--------------------------------------------\n")
-            f.write("Results from the Comparison of ETKF and LETKF\n")
-            f.write("versus the analytical posterior from the KF\n")
-            f.write("--------------------------------------------\n")
-            f.write("Mean RMSE EKTF  = " + str(avg_mean_rmse_etkfs) + "\n")
-            f.write("Mean RMSE LEKTF = " + str(avg_mean_rmse_letkfs) + "\n")
-            f.write("\n")
-            f.write("Cov Frobenius ETKF  = " + str(avg_cov_frob_etkfs) + "\n")
-            f.write("Cov Frobenius LETKF = " + str(avg_cov_frob_letkfs) + "\n")
-            f.write("\n")
+            if as_table is None:
+                file = "experiment_files/experiment_" + timestamp + "/results_" + self.result_timestamp
 
-            for p in range(self.N_poi):
-                f.write("ECDF Dist at PoI" + str(p) + " ETKF  = " + str(avg_ecdf_err_etkfs[p]) + "\n")
-                f.write("ECDF Dist at PoI" + str(p) + " LETKF = " + str(avg_ecdf_err_letkfs[p]) + "\n")
+                f = open(file, "a")
+                f.write("--------------------------------------------\n")
+                f.write("Results from the Comparison of ETKF and LETKF\n")
+                f.write("versus the analytical posterior from the KF\n")
+                f.write("--------------------------------------------\n")
+                f.write("Mean RMSE EKTF  = " + str(avg_mean_rmse_etkfs) + "\n")
+                f.write("Mean RMSE LEKTF = " + str(avg_mean_rmse_letkfs) + "\n")
+                f.write("\n")
+                f.write("Cov Frobenius ETKF  = " + str(avg_cov_frob_etkfs) + "\n")
+                f.write("Cov Frobenius LETKF = " + str(avg_cov_frob_letkfs) + "\n")
+                f.write("\n")
+
+                for p in range(self.N_poi):
+                    f.write("ECDF Dist at PoI" + str(p) + " ETKF  = " + str(avg_ecdf_err_etkfs[p]) + "\n")
+                    f.write("ECDF Dist at PoI" + str(p) + " LETKF = " + str(avg_ecdf_err_letkfs[p]) + "\n")
+            
+            else:
+                result_timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+                file = "experiment_files/experiment_" + timestamp + "/results_" + result_timestamp
+
+                table = np.vstack((as_table, \
+                    self.mean_rmse_etkfs, \
+                    self.mean_rmse_letkfs, \
+                    self.cov_frob_etkfs,\
+                    self.cov_frob_letkfs))
+
+                for p in range(self.N_poi):
+                    table = np.vstack((table, self.ecdf_err_etkfs[p], self.ecdf_err_letkfs[p]))
+
+                np.savetxt(file, table)
--- run_FilteringComparison.py
+++ run_FilteringComparison.py
@@ -26,79 +26,98 @@ print("done\n")
 
 # Repeated ensemble runs 
 
-N_e = 100
+N_es = [25, 50, 100, 250, 1000]
 
-trials_truth = 100
-trials_init  = 1
 pois = [[0,0], [25,15], [0,1]]
 
-runningWriter = RunningWriter.RunningWriter(trials=trials_truth*trials_init, N_poi=len(pois))
-RunningWriter.RunningWriter.header2file(N_e, trials_truth, trials_init, timestamp)
+runningModelWriter = RunningWriter.RunningWriter(trials=len(N_es), N_poi=len(pois))
 
-for trail_truth in range(trials_truth):
-    # Truth
-    print("New true observations", trail_truth)
-    observation.clear_observations()
+for trial_model in range(runningModelWriter.trials):
+    print("Changing the model! Set up ", trial_model)
 
-    statistics_truth = Statistics.Statistics(simulator, 1)
-    statistics_truth.set_prior(prior_args)
+    N_e = N_es[trial_model]
 
-    for t in range(10):
-        statistics_truth.propagate(25)
-        observation.observe(statistics_truth.mean)
-    
-    # KF 
-    print("KF DA")
-    statistics_kf = Statistics.Statistics(simulator)
-    statistics_kf.set_prior(prior_args)
+    trials_truth = 20
+    trials_init  = 5
 
-    kalmanFilter = KalmanFilter.Kalman(statistics_kf, observation)
+    runningWriter = RunningWriter.RunningWriter(trials=trials_truth*trials_init, N_poi=len(pois))
+    runningWriter.header2file(N_e, trials_truth, trials_init, timestamp)
 
-    for t in range(observation.N_obs):
-        statistics_kf.propagate(25)
-        kalmanFilter.filter(statistics_kf.mean, statistics_kf.cov, observation.obses[t])
+    for trail_truth in range(trials_truth):
+        # Truth
+        print("New true observations", trail_truth)
+        observation.clear_observations()
 
+        statistics_truth = Statistics.Statistics(simulator, 1)
+        statistics_truth.set_prior(prior_args)
 
-    for trial_init in range(trials_init):
-        print("Ensemble init ", trial_init)
-
-        # ETKF 
-        print("ETKF DA")
-        statistics_etkf = Statistics.Statistics(simulator, N_e)
-        statistics_etkf.set_prior(prior_args)
-
-        etkFilter = ETKalmanFilter.ETKalman(statistics_etkf, observation)
-
-        for t in range(observation.N_obs):
-            statistics_etkf.propagate(25)
-            etkFilter.filter(statistics_etkf.ensemble.ensemble, observation.obses[t])
-
-        # LETKF
-        print("LETKF DA")
-        statistics_letkf = Statistics.Statistics(simulator, N_e)
-        statistics_letkf.set_prior(prior_args)
-
-        scale_r = 8
-        sletkFilter = SLETKalmanFilter.SLETKalman(statistics_letkf, observation, scale_r)
-
-        for t in range(observation.N_obs):
-            statistics_letkf.propagate(25)
-            sletkFilter.filter(statistics_letkf.ensemble.ensemble, observation.obses[t])
-
-        # Comparison
-        print("Comparing")
-        trial = trail_truth*trials_init + trial_init
-        comparer = Comparer.Comparer(statistics_kf, statistics_etkf, statistics_letkf)
-
-        mean_rmse_kf, runningWriter.mean_rmse_etkfs[trial], runningWriter.mean_rmse_letkfs[trial] = comparer.mean_rmse()
-        cov_frob_kf, runningWriter.cov_frob_etkfs[trial], runningWriter.cov_frob_letkfs[trial] = comparer.cov_frobenius_dist()
-
-        for p in range(len(pois)):
-            comparer.set_poi(pois[p])
+        for t in range(10):
+            statistics_truth.propagate(25)
+            observation.observe(statistics_truth.mean)
         
-        for p in range(len(pois)):
-            runningWriter.ecdf_err_etkfs[p][trial], runningWriter.ecdf_err_letkfs[p][trial] = comparer.poi_ecdf_err(p)
-    
-        print("done\n")
+        # KF 
+        print("KF DA")
+        statistics_kf = Statistics.Statistics(simulator)
+        statistics_kf.set_prior(prior_args)
 
-runningWriter.results2file(timestamp)
+        kalmanFilter = KalmanFilter.Kalman(statistics_kf, observation)
+
+        for t in range(observation.N_obs):
+            statistics_kf.propagate(25)
+            kalmanFilter.filter(statistics_kf.mean, statistics_kf.cov, observation.obses[t])
+
+
+        for trial_init in range(trials_init):
+            print("Ensemble init ", trial_init)
+
+            # ETKF 
+            print("ETKF DA")
+            statistics_etkf = Statistics.Statistics(simulator, N_e)
+            statistics_etkf.set_prior(prior_args)
+
+            etkFilter = ETKalmanFilter.ETKalman(statistics_etkf, observation)
+
+            for t in range(observation.N_obs):
+                statistics_etkf.propagate(25)
+                etkFilter.filter(statistics_etkf.ensemble.ensemble, observation.obses[t])
+
+            # LETKF
+            print("LETKF DA")
+            statistics_letkf = Statistics.Statistics(simulator, N_e)
+            statistics_letkf.set_prior(prior_args)
+
+            scale_r = 8
+            sletkFilter = SLETKalmanFilter.SLETKalman(statistics_letkf, observation, scale_r)
+
+            for t in range(observation.N_obs):
+                statistics_letkf.propagate(25)
+                sletkFilter.filter(statistics_letkf.ensemble.ensemble, observation.obses[t])
+
+            # Comparison
+            print("Comparing")
+            trial = trail_truth*trials_init + trial_init
+            comparer = Comparer.Comparer(statistics_kf, statistics_etkf, statistics_letkf)
+
+            mean_rmse_kf, runningWriter.mean_rmse_etkfs[trial], runningWriter.mean_rmse_letkfs[trial] = comparer.mean_rmse()
+            cov_frob_kf, runningWriter.cov_frob_etkfs[trial], runningWriter.cov_frob_letkfs[trial] = comparer.cov_frobenius_dist()
+
+            for p in range(len(pois)):
+                comparer.set_poi(pois[p])
+            
+            for p in range(len(pois)):
+                runningWriter.ecdf_err_etkfs[p][trial], runningWriter.ecdf_err_letkfs[p][trial] = comparer.poi_ecdf_err(p)
+        
+            print("done\n")
+
+
+    runningModelWriter.mean_rmse_etkfs[trial_model], \
+    runningModelWriter.mean_rmse_letkfs[trial_model], \
+    runningModelWriter.cov_frob_etkfs[trial_model], \
+    runningModelWriter.cov_frob_letkfs[trial_model], \
+    runningModelWriter.ecdf_err_etkfs[:,trial_model], \
+    runningModelWriter.ecdf_err_letkfs[:,trial_model]\
+    = runningWriter.results()
+
+    runningWriter.results2file(timestamp)
+
+runningModelWriter.results2file(timestamp, N_es)
